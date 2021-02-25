83f71648a4ff7e20ab9e5dd9326d81d97b4db3fd
Jeffrey Rose <jtr219@outlook.com>
2021-02-25 13:56:54 -0500

    sql template for pagination after groupBy query

--- sql/queries/sales_by_title.sql
+++ sql/queries/sales_by_title.sql
@@ -24,3 +24,34 @@ SELECT sum(global_sales) AS global_sales,
 FROM games
 GROUP BY title
 HAVING title = :title;
+-- find single with filter options
+SELECT *
+FROM games
+WHERE title = :title -- ot Like?
+    AND year_of_release = :year -- what about range?
+    AND console = :console
+    AND genre = :genre
+    AND rating = :rating
+    AND publisher = :publisher
+LIMIT :limit;
+-- with pagination
+--this works, but there has to be a leaner way to do it
+SELECT *
+FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (
+                ORDER BY tempo.global_sales DESC
+            ) as row_n
+        FROM (
+                SELECT sum(global_sales) AS global_sales,
+                    sum(na_sales) AS na_sales,
+                    sum(eu_sales) AS eu_sales,
+                    sum(jp_sales) AS jp_sales,
+                    sum(other_sales) AS other_sales,
+                    title
+                FROM games
+                GROUP BY title
+                ORDER BY global_sales DESC
+            ) as tempo
+    ) as with_row_n
+where with_row_n.row_n between 1 and 10
