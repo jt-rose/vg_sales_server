0415d158e263e76eaee5080ff7ebe7e047216583
Jeffrey Rose <jtr219@outlook.com>
2021-04-22 00:11:22 -0400

    added limit to query complexity

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,52 @@ import { Games } from './resolvers/Games'
 import { Consoles } from './resolvers/Consoles'
 import { ConsoleMakers } from './resolvers/ConsoleMakers'
 import { buildSchema } from 'type-graphql'
+import {
+  getComplexity,
+  fieldExtensionsEstimator,
+  simpleEstimator,
+} from 'graphql-query-complexity'
 
 const main = async () => {
   const app = express()
-
+  const schema = await buildSchema({
+    resolvers: [Games, Consoles, ConsoleMakers],
+    validate: false,
+    // automatically create `schema.gql` file with schema definition in project's working directory
+    emitSchemaFile: true,
+  })
   const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [Games, Consoles, ConsoleMakers],
-      validate: false,
-      // automatically create `schema.gql` file with schema definition in project's working directory
-      emitSchemaFile: true,
-    }),
+    schema,
     // plugins: [apolloLogger],
+    plugins: [
+      {
+        requestDidStart: () => ({
+          didResolveOperation({ request, document }) {
+            // limit query complexity
+            const complexity = getComplexity({
+              schema,
+              operationName: request.operationName,
+              query: document,
+              variables: request.variables,
+              estimators: [
+                // Using fieldExtensionsEstimator is mandatory to make it work with type-graphql.
+                fieldExtensionsEstimator(),
+                simpleEstimator({ defaultComplexity: 1 }),
+              ],
+            })
+            if (complexity > 200) {
+              console.log(
+                `query rejected for having excess complexity of ${complexity}`
+              )
+              throw new Error(
+                `The server has rejected this request for having too great a complexity. Please revise as a more limited number of queries.`
+              )
+            }
+            // And here we can e.g. subtract the complexity point from hourly API calls limit.
+          },
+        }),
+      },
+    ],
     // context
   })
 
